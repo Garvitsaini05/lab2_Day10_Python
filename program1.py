03a19c263cc4c448c6ebb8deefd3fed1ca4d9dc4
Garivt Saini <garvitsaini05@gmail.com>
2023-12-07 16:10:29 -0500

    V4

--- program1.py
+++ program1.py
@@ -1,16 +1,21 @@
 # Menu Driven Program-------------------///////////////////
 
 shoppingCart={}
-
 print("")
 print("!!.....WELCOME TO THE AMANDO SHOPPING SITE.....!!")
 print("")
 enter=input("FOR MENU Press ENTER.....!!")
-if enter == "":
-    print(" 1. Add a product to the cart.\n 2. Search for a product.\n 3. Delete a product from the cart.\n 4. Display the contents of the cart.\n 5. Display the contents of the cart.\n 6. Quit.\n ")
 
-print("Type the MENU NUMBER you want to perform...")
-WTD=int(input(":- "))
+while (enter!=6):
+
+
+    if enter == "":
+            print("!!....MENU....!!\n 1. Add a product to the cart.\n 2. Search for a product.\n 3. Delete a product from the cart.\n 4. Display the contents of the cart.\n 5. Purchase items.\n 6. Quit.\n ")
+    print("")
+    print("Type the MENU NUMBER you want to perform...")
+    WTD=int(input(":- "))
+    print("")
+
 
 #FOR-1--------------------------************************
 if WTD == 1:
@@ -21,12 +26,77 @@ if WTD == 1:
             print("SORRY, You can only enter 5 ITEMS at a time...!")
             break
         else:
-            x=input(f"Enter product{i}-name:- ")
-            y=input(f"Enter product{i}-price :- ")
-            z=input(f"Enter product{i}-brand :- ")
+            x=input(f"Enter product {i} & brand name :- ")
+            y=input(f"Enter product {i}-price :- ")
             shoppingCart.update({x:y})
             i=i+1
-print(shoppingCart)
+
+            print("")
+
 #FOR-2--------------------------************************
 if WTD == 2:
-    a=int(input("What to search :- "))
+    print("What to search..?\n 1. Whole Cart.\n 2. Specific Item.")
+    b=int(input(":- "))
+    i=1
+    while i == 1:
+        if b == 2:
+            p=input("Product-name :- ")
+            if p in shoppingCart:
+                print(p," : ",shoppingCart[p])
+            else:
+                print("No product exists with this name.!!!!!")
+        else:
+            print(shoppingCart)
+        i=i+1
+
+        print("")
+
+
+#FOR-3--------------------------************************
+if WTD == 3:
+    delete=input("DELETE SOMETHING.........??? (y/n):- ")
+    if delete == "y":
+        print("what to DELETE")
+        c=input(":- ")
+        if c in shoppingCart:
+            shoppingCart.pop(c)
+        else:
+            print("Product not found in cart")
+
+        print("Updated DATA")
+        print(shoppingCart)
+    else:
+        print("Okay")
+    print("")
+
+
+#FOR-4--------------------------************************
+if WTD == 4:
+    print("")
+    print(shoppingCart)
+
+#FOR-5--------------------------************************
+if WTD == 5:
+    d = input("Complete Purchase.....? (y/n) :- ")
+    if d == "y":
+        
+        print("Thank you for your business.")
+        del shoppingCart
+    elif d == "n":
+        print("Please continue shopping")
+    else:
+        print("Please answer either Y or N.")
+
+#FOR-6--------------------------************************
+if WTD == 6:
+    print("Terminating..........")
+
+
+
+
+    if enter == "":
+            print("!!....MENU....!!\n 1. Add a product to the cart.\n 2. Search for a product.\n 3. Delete a product from the cart.\n 4. Display the contents of the cart.\n 5. Purchase items.\n 6. Quit.\n ")
+    print("")
+    print("Type the MENU NUMBER you want to perform...")
+    WTD=int(input(":- "))
+    print("")
